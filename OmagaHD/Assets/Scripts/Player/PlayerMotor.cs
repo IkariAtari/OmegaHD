b4416cb76927f108ee1320c22fc56b86c0414420
IkariAtari <jouri96@hotmail.com>
2019-10-21 20:43:22 +0100

    Added Camera Rotation

---
+++ OmagaHD/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Omega.Player
+{
+    public class PlayerMotor : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera Cam;
+
+        [SerializeField]
+        private float Speed;
+
+        [SerializeField]
+        private float LookSensitivity;
+
+        private CharacterController Controller;
+
+        private void Start() 
+        {
+            Controller = GetComponent<CharacterController>();
+        }
+
+        private void Update() 
+        {
+            float xMovement = Input.GetAxisRaw("Horizontal");
+            float zMovement = Input.GetAxisRaw("Vertical");
+            float xRotation = Input.GetAxisRaw("Mouse Y");
+            float yRotation = Input.GetAxisRaw("Mouse X");
+            
+            Vector3 xVector = transform.right * xMovement * Time.deltaTime;
+            Vector3 zVector = transform.forward * zMovement * Time.deltaTime;
+
+            Vector3 xRotationVector = new Vector3(-xRotation, 0, 0) * LookSensitivity;
+            Vector3 yRotationVector = new Vector3(0, yRotation, 0) * LookSensitivity;
+
+            Vector3 Velocity = (xVector + zVector).normalized * Speed;
+
+            Move(Velocity);
+            Rotation(xRotationVector, yRotationVector);
+        }
+
+        private void Move(Vector3 Velocity)
+        {
+            Controller.Move(Velocity);
+        }
+
+        private void Rotation(Vector3 X, Vector3 Y)
+        {
+            this.transform.Rotate(transform.rotation * Y);
+
+            Cam.transform.Rotate(X);
+        }
+    }
+}
